afddaa0ae014f1cfef14d923bf20ee89e383793d
Bisha <ogonia07@mail.ru>
2025-09-23 08:01:06 +0300

    пустой коммит

--- homeWork9_YAR/src/Main.java
+++ homeWork9_YAR/src/Main.java
@@ -1,4 +1,3 @@
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,6 +6,5 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-
-      }
+    }
 }
